072405ea08be320da666c811f20d8156d6500028
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-17 14:48:18 -0300

    move django to backend folder

--- backend/ft_transcendence/settings.py
+++ backend/ft_transcendence/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	"pong.apps.PongConfig",
+    "pong.apps.PongConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
