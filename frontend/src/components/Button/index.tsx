14261d0764d153b99a9136deb8185b0803687b6e
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-18 19:01:09 -0300

    remove frontend framework

--- frontend/src/components/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { FunctionalComponent } from 'vue'
-import './button.css'
-
-interface Props { }
-
-export const Button: FunctionalComponent<Props> = (props, context) => {
-    return <button>{context.slots.default?.()}</button>
-}
