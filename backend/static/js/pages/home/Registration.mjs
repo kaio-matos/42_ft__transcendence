9176b73b9382dd7fc4a0c3ea1d270388d7f0ace4
Jacques <netinho@Jacquess-MBP.lan>
2024-07-30 14:42:39 -0300

    Regsitration partially working

--- backend/static/js/pages/home/Registration.mjs
+++ backend/static/js/pages/home/Registration.mjs
@@ -9,29 +9,44 @@ export const Registration = ({ onRegistration }) => {
   page.element.innerHTML = `
     <form id="registration-form" class="container d-flex flex-column gap-3 mx-auto">
       <t-input id="input-name" label="Name"></t-input>
+      <t-input id="input-email" label="Email"></t-input>
+      <t-input id="input-password" label="Password"></t-input>
       <t-button>Register</t-button>
     </form>
   `;
 
   let name = "";
-
+  let email = "";
+  let password = "";
   const form = page.element.querySelector("#registration-form");
-  const t_input = form.querySelector("#input-name");
+  const t_input_name = form.querySelector("#input-name");
+  const t_input_email = form.querySelector("#input-email");
+  const t_input_password = form.querySelector("#input-password");
+  ;
 
-  t_input.input.addEventListener("change", (e) => {
+
+  t_input_name.input.addEventListener("change", (e) => {
     name = e.target.value;
   });
 
+  t_input_email.input.addEventListener("change", (e) => {
+    email = e.target.value;
+  })
+
+  t_input_password.input.addEventListener("change", (e) => {
+    password = e.target.value;
+  })
+
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
-    t_input.clearErrors();
+    t_input_name.clearErrors();
 
     if (!name) {
-      t_input.addErrors(t().error_home_form_registration_missing_name);
+      t_input_name.addErrors(t().error_home_form_registration_missing_name);
       return;
     }
 
-    const player = await PlayerService.createPlayer({ name: name });
+    const player = await PlayerService.createPlayer({ name: name, email: email, password: password });
     onRegistration(player);
   });
 
