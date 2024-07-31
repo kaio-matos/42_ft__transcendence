8d7dffa2fc09b90f1efb5b895f52b37c3c76470d
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-30 22:22:12 -0300

    cleanup

--- backend/pong/routing.py
+++ backend/pong/routing.py
@@ -6,10 +6,6 @@ from pong.communication.TournamentCommunication import TournamentCommunicationCo
 
 websocket_urlpatterns = [
     re_path(r"ws/player/", PlayerCommunicationConsumer.as_asgi()),
-    # re_path(
-    #     r"ws/tournament/(?P<tournament_id>\w+)",
-    #     TournamentCommunicationConsumer.as_asgi(),
-    # ),
     path(
         r"ws/tournament/<slug:tournament_id>", TournamentCommunicationConsumer.as_asgi()
     ),
