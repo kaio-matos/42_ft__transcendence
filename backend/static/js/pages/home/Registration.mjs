f3c71a541b709ad11afd85547b2a194a75457ecd
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-31 15:39:40 -0300

    handle api errors and show to the client

--- backend/static/js/pages/home/Registration.mjs
+++ backend/static/js/pages/home/Registration.mjs
@@ -1,6 +1,7 @@
 import { Component } from "../../components/component.mjs";
 import { PlayerService } from "../../services/player.mjs";
 import { t } from "../../language.mjs";
+import { UnprocessableContentError } from "../../services/errors.mjs";
 
 /** @type {import("../../components/component.mjs").FunctionalComponent} */
 export const Registration = ({ onRegistration }) => {
@@ -9,16 +10,17 @@ export const Registration = ({ onRegistration }) => {
   page.element.innerHTML = `
     <form id="registration-form" class="container d-flex flex-column gap-3 mx-auto">
       <t-input id="input-name" label="Name"></t-input>
-      <t-input id="input-email" label="Email"></t-input>
-      <t-input id="input-password" label="Password"></t-input>
-      <t-button>Register</t-button>
+      <t-input id="input-email" label="Email" type="email"></t-input>
+      <t-input id="input-password" label="Password" type="password"></t-input>
+
+      <t-errors id="errors"></t-errors>
+      <t-button id="register-button">Register</t-button>
     </form>
 
     <p class="mt-5 d-flex gap-2">
       Already has an account?
       <t-button to="/login">Login now</t-button>
     </p>
-
   `;
 
   let name = "";
@@ -28,6 +30,9 @@ export const Registration = ({ onRegistration }) => {
   const t_input_name = form.querySelector("#input-name");
   const t_input_email = form.querySelector("#input-email");
   const t_input_password = form.querySelector("#input-password");
+  const t_errors = form.querySelector("#errors");
+  const t_button_register = form.querySelector("#register-button");
+
   t_input_name.input.addEventListener("change", (e) => {
     name = e.target.value;
   });
@@ -43,18 +48,30 @@ export const Registration = ({ onRegistration }) => {
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
     t_input_name.clearErrors();
+    t_input_email.clearErrors();
+    t_input_password.clearErrors();
+    t_errors.clearErrors();
 
-    if (!name) {
-      t_input_name.addErrors(t().error_home_form_registration_missing_name);
-      return;
+    try {
+      t_button_register.setLoading(true);
+      const player = await PlayerService.createPlayer({
+        name: name,
+        email: email,
+        password: password,
+      });
+      onRegistration(player);
+    } catch (error) {
+      if (error instanceof UnprocessableContentError) {
+        t_input_name.addErrors(error.data?.error?.name);
+        t_input_email.addErrors(error.data?.error?.email);
+        t_input_password.addErrors(error.data?.error?.password);
+        t_errors.addErrors(error.data?.error?._errors);
+      } else if (error instanceof RequestFailedError) {
+        t_errors.addErrors(error.data?.error?._errors);
+      }
+    } finally {
+      t_button_register.setLoading(false);
     }
-
-    const player = await PlayerService.createPlayer({
-      name: name,
-      email: email,
-      password: password,
-    });
-    onRegistration(player);
   });
 
   return page;
--- backend/static/js/pages/login/index.mjs
+++ backend/static/js/pages/login/index.mjs
@@ -1,6 +1,7 @@
 import { PlayerCommunication } from "../../communication/player.mjs";
 import { Component } from "../../components/component.mjs";
 import { router } from "../../index.mjs";
+import { RequestFailedError } from "../../services/errors.mjs";
 import { PlayerService } from "../../services/player.mjs";
 import { session } from "../../state/session.mjs";
 
@@ -11,9 +12,10 @@ export const Login = () => {
   page.element.innerHTML = `
     <form id="login-form" class="d-flex flex-column gap-3">
       <t-input id="input-email" label="Email"></t-input>
-      <t-input id="input-password" label="Password"></t-input>
+      <t-input id="input-password" label="Password" type="password"></t-input>
 
-      <t-button>Login</t-button>
+      <t-errors id="errors"></t-errors>
+      <t-button id="login-button">Login</t-button>
     </form>
     <p class="mt-5 d-flex gap-2">
       Does not have an account?
@@ -27,6 +29,8 @@ export const Login = () => {
   const form = page.element.querySelector("#login-form");
   const t_input_email = form.querySelector("#input-email");
   const t_input_password = form.querySelector("#input-password");
+  const t_errors = form.querySelector("#errors");
+  const t_button_login = form.querySelector("#login-button");
 
   t_input_email.input.addEventListener("change", (e) => {
     email = e.target.value;
@@ -40,14 +44,28 @@ export const Login = () => {
     event.preventDefault();
     t_input_email.clearErrors();
     t_input_password.clearErrors();
+    t_errors.clearErrors();
 
-    const player = await PlayerService.login({
-      email: email,
-      password: password,
-    });
-    session.player = player;
-    PlayerCommunication.Communication.connect();
-    router.navigate("/");
+    try {
+      t_button_login.setLoading(true);
+      const player = await PlayerService.login({
+        email: email,
+        password: password,
+      });
+      session.player = player;
+      PlayerCommunication.Communication.connect();
+      router.navigate("/");
+    } catch (error) {
+      if (error instanceof UnprocessableContentError) {
+        t_input_email.addErrors(error.data?.error?.email);
+        t_input_password.addErrors(error.data?.error?.password);
+        t_errors.addErrors(error.data?.error?._errors);
+      } else if (error instanceof RequestFailedError) {
+        t_errors.addErrors(error.data?.error?._errors);
+      }
+    } finally {
+      t_button_login.setLoading(false);
+    }
   });
 
   return page;
