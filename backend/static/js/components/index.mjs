a0a59fe41a557800e8c83918768c413d36af2a37
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-31 21:03:29 -0300

    create Toast component

--- backend/static/js/components/index.mjs
+++ backend/static/js/components/index.mjs
@@ -3,9 +3,11 @@ import { Errors } from "./Errors.mjs";
 import { Input } from "./Input.mjs";
 import { Loading } from "./Loading.mjs";
 import { PongCanvas } from "./PongCanvas/PongCanvas.mjs";
+import { Toast } from "./Toast.mjs";
 
 customElements.define("t-button", Button);
 customElements.define("t-input", Input);
 customElements.define("t-errors", Errors);
 customElements.define("t-loading", Loading);
 customElements.define("t-pong-canvas", PongCanvas);
+customElements.define("t-toast", Toast);
