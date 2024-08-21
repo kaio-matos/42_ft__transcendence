f01a04fcf1c5ff53be3358601b522ac878fd0a97
Thayna Habeck <thaynahabeck@gmail.com>
2024-08-21 20:10:53 -0300

    adjustment in certificates

--- .docker/api/entrypoint.sh
+++ .docker/api/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Create logs directory
+mkdir -p logs
+
+# Generate SSL certificate
+openssl req -x509 -newkey rsa:4096 \
+    -keyout ./certs/localhost.key -out ./certs/localhost.crt \
+    -days 365 -nodes -subj "/CN=transcendence"
+
 # Create translation files
 echo "Creating Tranlation Files"
 python manage.py makemessages --all
