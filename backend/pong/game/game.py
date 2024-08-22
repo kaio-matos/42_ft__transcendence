7a5fc7b09624f0b62cbf2585b83f0f76d0cd7d4e
Kaio Matos <kaio_matos22@hotmail.com>
2024-08-21 21:54:07 -0300

    fix backend and frontend integration for paddle sizes

--- backend/pong/game/game.py
+++ backend/pong/game/game.py
@@ -12,9 +12,10 @@ from .ball import Ball
 from .paddle import Paddle
 from ..models import Match, Player
 
+
 class Game:
     ball_size = {"width": 0.8, "height": 0.8}
-    ball_speed = 1.3 # Velocidade constante da bola
+    ball_speed = 1.3  # Velocidade constante da bola
     vertical_paddle_size = {"width": 1, "height": 20}
     horizontal_paddle_size = {"width": 20, "height": 1}
 
@@ -35,16 +36,25 @@ class Game:
         num_players = len(self.match.players.all())
         self.paddle_velocity = 1.5
 
-        placements = [GamePlayerPlacement.LEFT, GamePlayerPlacement.RIGHT, GamePlayerPlacement.TOP, GamePlayerPlacement.BOTTOM]
-        
+        placements = [
+            GamePlayerPlacement.LEFT,
+            GamePlayerPlacement.RIGHT,
+            GamePlayerPlacement.TOP,
+            GamePlayerPlacement.BOTTOM,
+        ]
+
         for i, player in enumerate(self.match.players.all()):
             placement = placements[i]
             paddle_size = self.get_paddle_size(placement)
             position = self.get_initial_position(placement, paddle_size)
-            
-            self.players[str(player.public_id)] = GamePlayer(placement, position, player.toDict())
-            self.paddle_sizes[str(player.public_id)] = paddle_size  # Armazena o tamanho do paddle separadamente
-        
+
+            self.players[str(player.public_id)] = GamePlayer(
+                placement, position, player.toDict()
+            )
+            self.paddle_sizes[str(player.public_id)] = (
+                paddle_size  # Armazena o tamanho do paddle separadamente
+            )
+
         self.reset_ball()
 
     def get_paddle_size(self, placement):
@@ -57,23 +67,27 @@ class Game:
         if placement == GamePlayerPlacement.LEFT:
             return Position(0, CANVAS_HEIGHT / 2 - paddle_size["height"] / 2)
         elif placement == GamePlayerPlacement.RIGHT:
-            return Position(CANVAS_WIDTH - paddle_size["width"], CANVAS_HEIGHT / 2 - paddle_size["height"] / 2)
+            return Position(
+                CANVAS_WIDTH - paddle_size["width"],
+                CANVAS_HEIGHT / 2 - paddle_size["height"] / 2,
+            )
         elif placement == GamePlayerPlacement.TOP:
             return Position(CANVAS_WIDTH / 2 - paddle_size["width"] / 2, 0)
         elif placement == GamePlayerPlacement.BOTTOM:
-            return Position(CANVAS_WIDTH / 2 - paddle_size["width"] / 2, CANVAS_HEIGHT - paddle_size["height"])
+            return Position(
+                CANVAS_WIDTH / 2 - paddle_size["width"] / 2,
+                CANVAS_HEIGHT - paddle_size["height"],
+            )
 
     def reset_ball(self):
         self.ball.position = Position(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2)
-        angle = random.uniform(-math.pi/4, math.pi/4)
+        angle = random.uniform(-math.pi / 4, math.pi / 4)
         self.ball.velocity = Position(
-            self.ball_speed * math.cos(angle),
-            self.ball_speed * math.sin(angle)
+            self.ball_speed * math.cos(angle), self.ball_speed * math.sin(angle)
         )
         if random.choice([True, False]):
             self.ball.velocity.x *= -1
-        self.last_paddle_hit = None 
-
+        self.last_paddle_hit = None
 
     def start_game(self):
         if not self.game_running:
@@ -85,7 +99,7 @@ class Game:
         while self.game_running:
             self.update_game_state()
             self.broadcast_game_state()
-            time.sleep(0.016)  
+            time.sleep(0.016)
 
     def update_game_state(self):
         self.move_ball()
@@ -118,7 +132,6 @@ class Game:
                 return True
         return False
 
-
     def check_paddle_collision(self, player_id: str, player: GamePlayer):
         paddle = Paddle(player.position)
         paddle_size = self.paddle_sizes[player_id]
@@ -133,35 +146,51 @@ class Game:
         paddle_top = paddle.position.y
         paddle_bottom = paddle.position.y + paddle_size["height"]
 
-        if ((ball_right >= paddle_left and ball_left <= paddle_right) and
-            (ball_bottom >= paddle_top and ball_top <= paddle_bottom)):
-            
+        if (ball_right >= paddle_left and ball_left <= paddle_right) and (
+            ball_bottom >= paddle_top and ball_top <= paddle_bottom
+        ):
+
             # Calcula o ponto de colisão relativo ao centro do paddle
-            if player.placement in [GamePlayerPlacement.LEFT, GamePlayerPlacement.RIGHT]:
-                relative_intersect_y = (paddle.position.y + paddle_size["height"] / 2) - ball_next_y
-                normalized_relative_intersect = relative_intersect_y / (paddle_size["height"] / 2)
+            if player.placement in [
+                GamePlayerPlacement.LEFT,
+                GamePlayerPlacement.RIGHT,
+            ]:
+                relative_intersect_y = (
+                    paddle.position.y + paddle_size["height"] / 2
+                ) - ball_next_y
+                normalized_relative_intersect = relative_intersect_y / (
+                    paddle_size["height"] / 2
+                )
                 bounce_angle = normalized_relative_intersect * (5 * math.pi / 12)
                 direction = -1 if self.ball.velocity.x > 0 else 1
-                self.ball.velocity.x = direction * self.ball_speed * math.cos(bounce_angle)
+                self.ball.velocity.x = (
+                    direction * self.ball_speed * math.cos(bounce_angle)
+                )
                 self.ball.velocity.y = self.ball_speed * -math.sin(bounce_angle)
             else:
-                relative_intersect_x = (paddle.position.x + paddle_size["width"] / 2) - ball_next_x
-                normalized_relative_intersect = relative_intersect_x / (paddle_size["width"] / 2)
+                relative_intersect_x = (
+                    paddle.position.x + paddle_size["width"] / 2
+                ) - ball_next_x
+                normalized_relative_intersect = relative_intersect_x / (
+                    paddle_size["width"] / 2
+                )
                 bounce_angle = normalized_relative_intersect * (5 * math.pi / 12)
                 direction = -1 if self.ball.velocity.y > 0 else 1
                 self.ball.velocity.x = self.ball_speed * -math.sin(bounce_angle)
-                self.ball.velocity.y = direction * self.ball_speed * math.cos(bounce_angle)
-            
+                self.ball.velocity.y = (
+                    direction * self.ball_speed * math.cos(bounce_angle)
+                )
+
             # Move a bola ligeiramente para fora do paddle para evitar colisões múltiplas
-            if player.placement == GamePlayerPlacement.LEFT:
-                self.ball.position.x = paddle_right + 0.01
-            elif player.placement == GamePlayerPlacement.RIGHT:
-                self.ball.position.x = paddle_left - self.ball_size["width"] - 0.01
-            elif player.placement == GamePlayerPlacement.TOP:
-                self.ball.position.y = paddle_bottom + 0.01
-            elif player.placement == GamePlayerPlacement.BOTTOM:
-                self.ball.position.y = paddle_top - self.ball_size["height"] - 0.01
-            
+            # if player.placement == GamePlayerPlacement.LEFT:
+            #     self.ball.position.x = paddle_right + 0.01
+            # elif player.placement == GamePlayerPlacement.RIGHT:
+            #     self.ball.position.x = paddle_left - self.ball_size["width"] - 0.01
+            # elif player.placement == GamePlayerPlacement.TOP:
+            #     self.ball.position.y = paddle_bottom + 0.01
+            # elif player.placement == GamePlayerPlacement.BOTTOM:
+            #     self.ball.position.y = paddle_top - self.ball_size["height"] - 0.01
+
             return True
         return False
 
@@ -169,26 +198,33 @@ class Game:
         game_player = self.players.get(str(player.public_id))
         if game_player:
             paddle_size = self.paddle_sizes[str(player.public_id)]
-            if game_player.placement in [GamePlayerPlacement.LEFT, GamePlayerPlacement.RIGHT]:
+            if game_player.placement in [
+                GamePlayerPlacement.LEFT,
+                GamePlayerPlacement.RIGHT,
+            ]:
                 if direction == GameDirection.UP:
                     new_y = game_player.position.y - self.paddle_velocity
                     game_player.position.y = max(new_y, 0)
                 elif direction == GameDirection.DOWN:
                     new_y = game_player.position.y + self.paddle_velocity
-                    game_player.position.y = min(new_y, CANVAS_HEIGHT - paddle_size["height"])
+                    game_player.position.y = min(
+                        new_y, CANVAS_HEIGHT - paddle_size["height"]
+                    )
             else:
                 if direction == GameDirection.LEFT:
                     new_x = game_player.position.x - self.paddle_velocity
                     game_player.position.x = max(new_x, 0)
                 elif direction == GameDirection.RIGHT:
                     new_x = game_player.position.x + self.paddle_velocity
-                    game_player.position.x = min(new_x, CANVAS_WIDTH - paddle_size["width"])
+                    game_player.position.x = min(
+                        new_x, CANVAS_WIDTH - paddle_size["width"]
+                    )
 
     def broadcast_game_state(self):
         self.match.broadcast_match(
             ws.WSResponse(ws.WSEvents.MATCH_UPDATE, self.toDict()),
         )
-    
+
     def hasFinished(self):
         # self.winner = self.match.players.first()
         # return True
@@ -201,7 +237,6 @@ class Game:
             "game": {
                 "players": [player.toDict() for player in self.players.values()],
                 "ball": self.ball.toDict(),
-                "paddle": {"size": self.vertical_paddle_size},
                 "is_running": self.game_running,
                 "last_paddle_hit": self.last_paddle_hit,
             },
